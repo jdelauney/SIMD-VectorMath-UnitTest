512b7b64b96d5ee6fcbad9b9655cf512fea2038a
peter <github@skel.biz>
2018-01-01 10:20:20 +0000

    missing files

---
+++ VectorClassesUnitTest/Vector3bComparatorTest.pas
@@ -0,0 +1,193 @@
+unit Vector3bComparatorTest;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testregistry, BaseTestCase,
+  native, GLZVectorMath;
+
+type
+
+  { TVector3bComparatorTest }
+
+  TVector3bComparatorTest = class(TByteVectorBaseTestCase)
+    published
+      procedure TestCompare;
+      procedure TestCompareFalse;
+      procedure TestOpAdd;
+      procedure TestOpAddByte;
+      procedure TestOpSub;
+      procedure TestOpSubByte;
+      procedure TestOpMul;
+      procedure TestOpMulByte;
+      procedure TestOpDiv;
+      procedure TestOpDivByte;
+      procedure TestOpEquality;
+      procedure TestOpNotEquals;
+      procedure TestOpAnd;
+      procedure TestOpAndByte;
+      procedure TestOpOr;
+      procedure TestOpOrByte;
+      procedure TestOpXor;
+      procedure TestOpXorByte;
+      procedure TestAsVector3f;
+      procedure TestSwizzle;
+
+  end;
+
+implementation
+
+procedure TVector3bComparatorTest.TestCompare;
+begin
+   AssertTrue('Test Values do not match'+nbt1.ToString+' --> '+abt1.ToString, Compare(nbt1,abt1));
+end;
+
+procedure TVector3bComparatorTest.TestCompareFalse;
+begin
+   AssertFalse('Test Values should not match'+nbt1.ToString+' --> '+abt2.ToString, Compare(nbt1,abt2));
+end;
+
+procedure TVector3bComparatorTest.TestOpAdd;
+begin
+   nbt3 := nbt1 + nbt2;
+   abt3 := abt1 + abt2;
+   AssertTrue('Vector + Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpAddByte;
+begin
+   nbt3 := nbt1 + b1;
+   abt3 := abt1 + b1;
+   AssertTrue('Vector + Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpSub;
+begin
+   nbt3 := nbt1 - nbt2;
+   abt3 := abt1 - abt2;
+   AssertTrue('Vector - Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpSubByte;
+begin
+   nbt3 := nbt1 - b1;
+   abt3 := abt1 - b1;
+   AssertTrue('Vector - Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpMul;
+begin
+   nbt3 := nbt1 * nbt2;
+   abt3 := abt1 * abt2;
+   AssertTrue('Vector * Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpMulByte;
+begin
+   nbt3 := nbt1 * b1;
+   abt3 := abt1 * b1;
+   AssertTrue('Vector * Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpDiv;
+begin
+   nbt3 := nbt1 Div nbt2;
+   abt3 := abt1 Div abt2;
+   AssertTrue('Vector Div Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpDivByte;
+begin
+   nbt3 := nbt1 Div b1;
+   abt3 := abt1 Div b1;
+   AssertTrue('Vector Div Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpEquality;
+begin
+  nb := nbt1 = nbt1;
+  ab := abt1 = abt1;
+  AssertTrue('Vector = do not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+  nb := nbt1 = nbt2;
+  ab := abt1 = abt2;
+  AssertTrue('Vector = should not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+end;
+
+procedure TVector3bComparatorTest.TestOpNotEquals;
+begin
+  nb := nbt1 = nbt1;
+  ab := abt1 = abt1;
+  AssertTrue('Vector <> do not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+  nb := nbt1 = nbt2;
+  ab := abt1 = abt2;
+  AssertTrue('Vector <> should not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+end;
+
+procedure TVector3bComparatorTest.TestOpAnd;
+begin
+   nbt3 := nbt1 and nbt2;
+   abt3 := abt1 and abt2;
+   AssertTrue('Vector And Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpAndByte;
+begin
+   nbt3 := nbt1 and b5;
+   abt3 := abt1 and b5;
+   AssertTrue('Vector And Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpOr;
+begin
+   nbt3 := nbt1 or nbt2;
+   abt3 := abt1 or abt2;
+   AssertTrue('Vector Or Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpOrByte;
+begin
+   nbt3 := nbt1 or b1;
+   abt3 := abt1 or b1;
+   AssertTrue('Vector Or Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpXor;
+begin
+   nbt3 := nbt1 xor nbt2;
+   abt3 := abt1 xor abt2;
+   AssertTrue('Vector Xor Vector no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestOpXorByte;
+begin
+   nbt3 := nbt1 xor b1;
+   abt3 := abt1 xor b1;
+   AssertTrue('Vector Xor Byte no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+procedure TVector3bComparatorTest.TestAsVector3f;
+var
+  nvt3: TNativeGLZVector3f;
+  avt3: TGLZVector3f;
+begin
+   nvt3 := nbt1.AsVector3f;
+   avt3 := abt1.AsVector3f;
+   AssertTrue('Vector AsVector3f no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nvt3,avt3));
+end;
+
+procedure TVector3bComparatorTest.TestSwizzle;
+begin
+   nbt3 := nbt1.Swizzle(swBRG);
+   abt3 := abt1.Swizzle(swBRG);
+   AssertTrue('Vector Swizzle no match'+nbt3.ToString+' --> '+abt3.ToString, Compare(nbt3,abt3));
+end;
+
+
+
+
+initialization
+  RegisterTest(REPORT_GROUP_VECTOR3B, TVector3bComparatorTest);
+end.
+
---
+++ VectorClassesUnitTest/Vector4bComparatorTest.pas
@@ -0,0 +1,355 @@
+unit Vector4bComparatorTest;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testregistry, BaseTestCase,
+  native, GLZVectorMath;
+
+type
+  { TVector4bComparatorTest }
+
+  TVector4bComparatorTest = class(TByteVectorBaseTestCase)
+    published
+      procedure TestCompare;
+      procedure TestCompareFalse;
+      procedure TestCreateBytes;
+      procedure TestCreatefrom3b;
+      procedure TestOpAdd;
+      procedure TestOpAddByte;
+      procedure TestOpSub;
+      procedure TestOpSubByte;
+      procedure TestOpMul;
+      procedure TestOpMulByte;
+      procedure TestOpDiv;
+      procedure TestOpDivByte;
+      procedure TestOpEquality;
+      procedure TestOpNotEquals;
+      procedure TestOpAnd;
+      procedure TestOpAndByte;
+      procedure TestOpOr;
+      procedure TestOpOrByte;
+      procedure TestOpXor;
+      procedure TestOpXorByte;
+      procedure TestDivideBy2;
+      procedure TestOpMin;
+      procedure TestOpMinByte;
+      procedure TestOpMax;
+      procedure TestOpMaxByte;
+      procedure TestOpClamp;
+      procedure TestOpClampByte;
+      procedure TestMulAdd;
+      procedure TestMulDiv;
+      procedure TestGetSwizzleMode;
+      procedure TestAsVector4f;
+      procedure TestShuffle;
+      procedure TestSwizzle;
+      procedure TestCombine;
+      procedure TestCombine2;
+      procedure TestCombine3;
+      procedure TestMinXYZComponent;
+      procedure TestMaxXYZComponent;
+  end;
+
+implementation
+
+
+{%region%----[ TVector4b Operators ]--------------------------------------------}
+procedure TVector4bComparatorTest.TestCompare;
+begin
+   AssertTrue('Test Values do not match'+nbf1.ToString+' --> '+abf1.ToString, Compare(nbf1,abf1));
+end;
+
+procedure TVector4bComparatorTest.TestCompareFalse;
+begin
+   AssertFalse('Test Values should not match'+nbf1.ToString+' --> '+abf2.ToString, Compare(nbf1,abf2));
+end;
+
+procedure TVector4bComparatorTest.TestCreateBytes;
+begin
+   nbf3.Create(b1,b2,b3,b7);
+   abf3.Create(b1,b2,b3,b7);
+   AssertTrue('Vector Create no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestCreatefrom3b;
+begin
+   nbf3.Create(nbt1,b7);
+   abf3.Create(abt1,b7);
+   AssertTrue('Vector Create no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpAdd;
+begin
+   nbf3 := nbf1 + nbf2;
+   abf3 := abf1 + abf2;
+   AssertTrue('Vector + Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpAddByte;
+begin
+   nbf3 := nbf1 + b1;
+   abf3 := abf1 + b1;
+   AssertTrue('Vector + Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpSub;
+begin
+   nbf3 := nbf1 - nbf2;
+   abf3 := abf1 - abf2;
+   AssertTrue('Vector - Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpSubByte;
+begin
+   nbf3 := nbf1 - b1;
+   abf3 := abf1 - b1;
+   AssertTrue('Vector - Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpMul;
+begin
+   nbf3 := nbf1 * nbf2;
+   abf3 := abf1 * abf2;
+   AssertTrue('Vector * Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpMulByte;
+begin
+   nbf3 := nbf1 * b1;
+   abf3 := abf1 * b1;
+   AssertTrue('Vector * Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpDiv;
+begin
+   nbf3 := nbf1 Div nbf2;
+   abf3 := abf1 Div abf2;
+   AssertTrue('Vector Div Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpDivByte;
+begin
+   nbf3 := nbf1 Div b1;
+   abf3 := abf1 Div b1;
+   AssertTrue('Vector Div Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpEquality;
+begin
+  nb := nbf1 = nbf1;
+  ab := abf1 = abf1;
+  AssertTrue('Vector = do not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+  nb := nbf1 = nbf2;
+  ab := abf1 = abf2;
+  AssertTrue('Vector = should not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+end;
+
+procedure TVector4bComparatorTest.TestOpNotEquals;
+begin
+  nb := nbf1 = nbf1;
+  ab := abf1 = abf1;
+  AssertTrue('Vector <> do not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+  nb := nbf1 = nbf2;
+  ab := abf1 = abf2;
+  AssertTrue('Vector <> should not match'+nb.ToString+' --> '+ab.ToString, (nb = ab));
+end;
+
+procedure TVector4bComparatorTest.TestOpAnd;
+begin
+   nbf3 := nbf1 and nbf2;
+   abf3 := abf1 and abf2;
+   AssertTrue('Vector And Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpAndByte;
+begin
+   nbf3 := nbf1 and b5;
+   abf3 := abf1 and b5;
+   AssertTrue('Vector And Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpOr;
+begin
+   nbf3 := nbf1 or nbf2;
+   abf3 := abf1 or abf2;
+   AssertTrue('Vector Or Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpOrByte;
+begin
+   nbf3 := nbf1 or b1;
+   abf3 := abf1 or b1;
+   AssertTrue('Vector Or Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpXor;
+begin
+   nbf3 := nbf1 xor nbf2;
+   abf3 := abf1 xor abf2;
+   AssertTrue('Vector Xor Vector no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpXorByte;
+begin
+   nbf3 := nbf1 xor b1;
+   abf3 := abf1 xor b1;
+   AssertTrue('Vector Xor Byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+
+{%endregion%}
+
+{%region%----[ TVector4b Functions ]--------------------------------------------}
+
+
+procedure TVector4bComparatorTest.TestDivideBy2;
+begin
+   nbf3 := nbf1.DivideBy2;
+   abf3 := abf1.DivideBy2;
+   AssertTrue('Vector DivideBy2 no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpMin;
+begin
+   nbf3 := nbf1.Min(nbf2);
+   abf3 := abf1.Min(abf2);
+   AssertTrue('Vector Min no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpMinByte;
+begin
+   nbf3 := nbf1.Min(b2);
+   abf3 := abf1.Min(b2);
+   AssertTrue('Vector Min byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+
+procedure TVector4bComparatorTest.TestOpMax;
+begin
+   nbf3 := nbf1.Max(nbf2);
+   abf3 := abf1.Max(abf2);
+   AssertTrue('Vector Max no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpMaxByte;
+begin
+   nbf3 := nbf1.Max(b2);
+   abf3 := abf1.Max(b2);
+   AssertTrue('Vector Max byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpClamp;
+begin
+   nbf4 := nbf1.Swizzle(swAGRB);
+   abf4 := abf1.Swizzle(swAGRB);
+   nbf3 := nbf1.Clamp(nbf2, nbf4);
+   abf3 := abf1.Clamp(abf2, abf4);
+   AssertTrue('Vector Clamp no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestOpClampByte;
+begin
+   nbf3 := nbf1.Clamp(b2, b5);
+   abf3 := abf1.Clamp(b2, b5);
+   AssertTrue('Vector Clamp byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestMulAdd;
+begin
+   nbf4 := nbf1.Swizzle(swAGRB);
+   abf4 := abf1.Swizzle(swAGRB);
+   nbf3 := nbf1.MulAdd(nbf2, nbf4);
+   abf3 := abf1.MulAdd(abf2, abf4);
+   AssertTrue('Vector MulAdd no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+
+end;
+
+procedure TVector4bComparatorTest.TestMulDiv;
+begin
+   nbf3 := nbf1.MulDiv(b2, b5);
+   abf3 := abf1.MulDiv(b2, b5);
+   AssertTrue('Vector MulDiv byte no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestGetSwizzleMode;
+var
+  nsw,asw: TGLZVector4SwizzleRef;
+begin
+   nsw := nbf1.GetSwizzleMode;
+   asw := abf1.GetSwizzleMode;
+   AssertEquals('Vector GetSwizzleMode no match', ord(nsw),ord(asw));
+end;
+
+procedure TVector4bComparatorTest.TestAsVector4f;
+var
+  nvt3: TNativeGLZVector4f;
+  avt3: TGLZVector4f;
+begin
+   nvt3 := nbf1.AsVector4f;
+   avt3 := abf1.AsVector4f;
+   AssertTrue('Vector AsVector3f no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nvt3,avt3));
+end;
+
+
+procedure TVector4bComparatorTest.TestShuffle;
+begin
+   nbf3 := nbf1.Shuffle(1,2,3,0);
+   abf3 := abf1.Shuffle(1,2,3,0);
+   AssertTrue('Vector Shuffle no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestSwizzle;
+begin
+   nbf3 := nbf1.Swizzle(swAGRB);
+   abf3 := abf1.Swizzle(swAGRB);
+   AssertTrue('Vector Swizzle no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestCombine;
+begin
+   nbf3 := nbf1.Combine(nbf2, b1);
+   abf3 := abf1.Combine(abf2, b1);
+   AssertTrue('Vector Combine no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestCombine2;
+begin
+   nbf3 := nbf1.Combine2(nbf2, b1, b2);
+   abf3 := abf1.Combine2(abf2, b1, b2);
+   AssertTrue('Vector Combine2 no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+end;
+
+procedure TVector4bComparatorTest.TestCombine3;
+begin
+   nbf4 := nbf1.Swizzle(swAGRB);
+   abf4 := abf1.Swizzle(swAGRB);
+   nbf3 := nbf1.Combine3(nbf2, nbf4, b1, b2, b3);
+   abf3 := abf1.Combine3(abf2, abf4, b1, b2, b3);
+   AssertTrue('Vector Combine3 no match'+nbf3.ToString+' --> '+abf3.ToString, Compare(nbf3,abf3));
+
+end;
+
+procedure TVector4bComparatorTest.TestMinXYZComponent;
+begin
+  b4 := nbf1.MinXYZComponent;
+  b8 := abf1.MinXYZComponent;
+  AssertEquals('Vector MinXYZComponent no match', b4, b8);
+end;
+
+procedure TVector4bComparatorTest.TestMaxXYZComponent;
+begin
+  b4 := nbf1.MaxXYZComponent;
+  b8 := abf1.MaxXYZComponent;
+  AssertEquals('Vector MaxXYZComponent no match', b4, b8);
+end;
+
+
+{%endregion%}
+
+initialization
+  RegisterTest(REPORT_GROUP_VECTOR4B, TVector4bComparatorTest);
+end.
+
