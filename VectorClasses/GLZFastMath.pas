b30e8dd25f13b162ff4648e3e1c5c029dc6a086f
jdelauney <delauney.jerome@gmail.com>
2018-01-14 17:33:15 +0100

    Add Fast Log, Exp, Sqrt, InvSqrt,... functions

--- VectorClasses/GLZFastMath.pas
+++ VectorClasses/GLZFastMath.pas
@@ -85,19 +85,54 @@ function RemezSin(val:Single): Single;
 
 {%endregion%}
 
+Function FastSinc(x: Single): Single;
 
-
+Function FastTan(x:Single):Single;
+Function FastArcTan(x:Single):Single;
 { Fast ArcTan2 approximation, about 0.07 rads accuracy. }
-Function FastArcTangent2(y, x: Single): Single;
+Function FastArcTan2(y, x: Single): Single;
 
 Function FastArcSine(Const x: Single): Single;
+Function FastArcCosine(const X: Single): Single;
 
 Function FastInvSqrt(Const Value: Single): Single;
+Function FastSqrt(Const Value: Single): Single;
+
+Function FastLn(Const X: Single): Single;
+
+Function FastExp(x:Single):Single;
+
+Function FastLog(x:Single):Single;
+
+Function FastLog2(X: Single): Single;
+
+Function FastLog10(X: Single): Single;
+
+Function FastLDExp(x: Single; N: Integer): Single;
+
+Function FastLNXP1(Const x: Single): Single;
+
+Function FastAbs(f:single):single;
+
+Function FastNeg(f:single):single;
+
+Function FastSign(f:single):longint;
+
+function FastPower(i:single;n:integer):single;
+
+
+// a passer dans glz math
+Function RoundInt(V: Single): Single;
+
+Function IsInRange(const X, a, b: Single): Boolean;
+
+
 
 implementation
 
 // On defini quelques valeurs en vue d'optimiser les calculs
 Const
+  _cInfinity = 1e1000;
   _EpsilonFuzzFactor = 1000;
   _EpsilonXTResolution = 1E-19 * _EpsilonFuzzFactor;
   _cPI: double = 3.1415926535897932384626433832795; //3.141592654;
@@ -175,7 +210,70 @@ Const
 Initialization
 
 
+{Vec4 VSin(const Vec4& x)
 
+Vec4 c1 = VReplicate(-1.f/6.f);
+Vec4 c2 = VReplicate(1.f/120.f);
+Vec4 c3 = VReplicate(-1.f/5040.f);
+Vec4 c4 = VReplicate(1.f/362880);
+Vec4 c5 = VReplicate(-1.f/39916800);
+Vec4 c6 = VReplicate(1.f/6227020800);
+Vec4 c7 = VReplicate(-1.f/1307674368000);
+
+Vec4 res =	x +
+c1*x*x*x +
+c2*x*x*x*x*x +
+c3*x*x*x*x*x*x*x +
+c4*x*x*x*x*x*x*x*x*x +
+c5*x*x*x*x*x*x*x*x*x*x*x +
+c6*x*x*x*x*x*x*x*x*x*x*x*x*x +
+c7*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x;
+
+return (res);
+
+
+//-----------
+
+Vec4 c1 = VReplicate(-1.f/6.f);
+Vec4 c2 = VReplicate(1.f/120.f);
+Vec4 c3 = VReplicate(-1.f/5040.f);
+Vec4 c4 = VReplicate(1.f/362880);
+Vec4 c5 = VReplicate(-1.f/39916800);
+Vec4 c6 = VReplicate(1.f/6227020800);
+Vec4 c7 = VReplicate(-1.f/1307674368000);
+
+Vec4 tmp0 = x;
+Vec4 x3 = x*x*x;
+Vec4 tmp1 = c1*x3;
+Vec4 res = tmp0 + tmp1;
+
+Vec4 x5 = x3*x*x;
+tmp0 = c2*x5;
+res	= res + tmp0;
+
+Vec4 x7 = x5*x*x;
+tmp0 = c3*x7;
+res	= res + tmp0;
+
+Vec4 x9 = x7*x*x;
+tmp0 = c4*x9;
+res	= res + tmp0;
+
+Vec4 x11 = x9*x*x;
+tmp0 = c5*x11;
+res	= res + tmp0;
+
+Vec4 x13 = x11*x*x;
+tmp0 = c6*x13;
+res	= res + tmp0;
+
+Vec4 x15 = x13*x*x;
+tmp0 = c7*x15;
+res	= res + tmp0;
+
+return (res);
+
+}
 
 
 end.
