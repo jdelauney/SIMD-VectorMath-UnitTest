f86a18ad38e00c338f9d79d5879df7584b93d2ee
jdelauney <delauney.jerome@gmail.com>
2018-01-23 23:44:56 +0100

    Updated doc comments for TGLZVectorHelper

--- VectorClasses/GLZVectorMath.pas
+++ VectorClasses/GLZVectorMath.pas
@@ -943,14 +943,12 @@ type
 
     // Returns the conjugate of a quaternion
     function Conjugate : TGLZQuaternion;
-    //procedure pConjugate;
 
     // Returns the magnitude of the quaternion
     function Magnitude : Single;
 
     // Normalizes the given quaternion
     function Normalize : TGLZQuaternion;
-    //procedure pNormalize;
 
     { Returns quaternion product qL * qR.
        Note: order is important!
@@ -1041,6 +1039,7 @@ type
   public
     procedure Create(const AValue: TGLZVector);overload;
     procedure Create(const AMin, AMax: TGLZVector);overload;
+
     { : Extract the AABB information from a BB. }
     procedure Create(const ABB: TGLZBoundingBox);
 
@@ -1112,7 +1111,14 @@ type
 
   TGLZVectorHelper = record helper for TGLZVector
   public
-    // Returns  given vector rotated around arbitery axis (alpha is in rad, , use Pure Math Model)
+    { : Returns given vector rotated around arbitrary axis (alpha is in rad, , use Pure Math Model)
+      We are using right hand rule coordinate system
+      Positive rotations are anticlock wise.
+      - Axis orientation to view positives in Upper Right quadrant [as graph axes]
+        - with +Z pointing at you (as screen) positive X is to the right positive Y is Up
+        - with +Y pointing at you positive Z is to the right positive X is Up
+        - with +X pointing at you positive Y is to the left positive Z is up
+    }
     function Rotate(constref axis : TGLZVector; angle : Single):TGLZVector;
     // Returns given vector rotated around the X axis (alpha is in rad, use Pure Math Model)
     function RotateWithMatrixAroundX(alpha : Single) : TGLZVector;
@@ -1121,12 +1127,22 @@ type
     // Returns given vector rotated around the Z axis (alpha is in rad, use Pure Math Model)
     function RotateWithMatrixAroundZ(alpha : Single) : TGLZVector;
 
-    { note : used in places such as File 3DS, particles,.... }
-    // Returns given vector rotated around the X axis (alpha is in rad)
+    { : Returns given vector rotated around the X axis (alpha is in rad)
+      - With +X pointing at you positive Y is to the left positive Z is Up.
+        Positive rotation around x, Y becomes negative
+    }
     function RotateAroundX(alpha : Single) : TGLZVector;
-    // Returns given vector rotated around the Y axis (alpha is in rad)
+
+    { : Returns given vector rotated around the Y axis (alpha is in rad)
+      - With +Y pointing at you positive Z is to the right positive X is Up.
+        Positive rotation around y, Z becomes negative
+    }
     function RotateAroundY(alpha : Single) : TGLZVector;
-    // Returns given vector rotated around the Z axis (alpha is in rad)
+
+    { : Returns given vector rotated around the Z axis (alpha is in rad)
+      - With +Z pointing at you (as screen) positive X is to the right positive Y is Up.
+        Positive rotation around z, X becomes negative
+    }
     function RotateAroundZ(alpha : Single) : TGLZVector;
 
     // Self is the point
@@ -1143,8 +1159,10 @@ type
     //function PointLineClosestPoint(const linePoint, lineDirection : TGLZVector) : TGLZVector;
     { Computes algebraic distance between point and line.}
     //function PointLineDistance(const linePoint, lineDirection : TGLZVector) : Single;
+
     { Extracted from Camera.MoveAroundTarget(pitch, turn). pitch and turn in deg }
     function MoveAround(constref AMovingObjectUp, ATargetPosition: TGLZVector; pitchDelta, turnDelta: Single): TGLZVector;
+
     { AOriginalPosition - Object initial position.
        ACenter - some point, from which is should be distanced.
 
@@ -1156,17 +1174,59 @@ type
 
     function AverageNormal4(constref up, left, down, right: TGLZVector): TGLZVector;
 
+    { : Extend a TGLZClipRect. Is a screen coord biased rect Top < Bottom, Left < Right
+      (Left, Top, Right, Bottom: Single)
+    }
     function ExtendClipRect(vX, vY: Single) : TGLZClipRect;
 
+    {  : Implement a step function returning either zero or one.
+      Implements a step function returning one for each component of Self that is
+      greater than or equal to the corresponding component in the reference
+      vector B, and zero otherwise.
+      see : http://developer.download.nvidia.com/cg/step.html
+    }
     function Step(ConstRef B : TGLZVector4f):TGLZVector4f;
+
+    { : Returns a normal as-is if a vertex's eye-space position vector points in the opposite direction of a geometric normal, otherwise return the negated version of the normal
+      Self = Peturbed normal vector.
+      A = Incidence vector (typically a direction vector from the eye to a vertex).
+      B = Geometric normal vector (for some facet the peturbed normal belongs).
+      see : http://developer.download.nvidia.com/cg/faceforward.html
+    }
     function FaceForward(constref A, B: TGLZVector4f): TGLZVector4f;
+
+    { : Returns smallest integer not less than a scalar or each vector component.
+      Returns Self saturated to the range [0,1] as follows:
+
+      1) Returns 0 if Self is less than 0; else
+      2) Returns 1 if Self is greater than 1; else
+      3) Returns Self otherwise.
+
+      For vectors, the returned vector contains the saturated result of each element
+      of the vector Self saturated to [0,1].
+      see : http://developer.download.nvidia.com/cg/saturate.html
+    }
     function Saturate : TGLZVector4f;
+
+    { : Interpolate smoothly between two input values based on a third
+      Interpolates smoothly from 0 to 1 based on Self compared to a and b.
+      1) Returns 0 if Self < a < b or Self > a > b
+      1) Returns 1 if Self < b < a or Self > b > a
+      3) Returns a value in the range [0,1] for the domain [a,b].
+
+      if A = Self
+      The slope of Self.smoothstep(a,b) and b.smoothstep(a,b) is zero.
+
+      For vectors, the returned vector contains the smooth interpolation of each
+      element of the vector x.
+      see : http://developer.download.nvidia.com/cg/smoothstep.html
+    }
     function SmoothStep(ConstRef A,B : TGLZVector4f): TGLZVector4f;
   end;
 
 {%endregion%}
 
-{%region%----[ TGLZHmgPlaneHelper ]-----------------------------------------------}
+{%region%----[ TGLZHmgPlaneHelper ]---------------------------------------------}
 
   // for functions where we use types not declared before TGLZHmgPlane
   TGLZHmgPlaneHelper = record helper for TGLZHmgPlane
@@ -1174,6 +1234,7 @@ type
     function Contains(const TestBSphere: TGLZBoundingSphere): TGLZSpaceContains;
     function PlaneContains(const Location, Normal: TGLZVector; const TestBSphere: TGLZBoundingSphere): TGLZSpaceContains;
   end;
+
 {%endregion%}
 
 {%region%----[ TGLZMatrixHelper ]-----------------------------------------------}
